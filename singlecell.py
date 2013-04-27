60ea6fae882228bdee9ca39d666163de8271ec88
MinRK <benjaminrk@gmail.com>
2013-04-27 09:42:28 -0700

    remove no-longer-necessary hacks
    use the cleaner `settings` recently introduced

--- singlecell.py
+++ singlecell.py
@@ -28,6 +28,13 @@ ioloop.install()
 from tornado import httpserver
 from tornado import web
 
+try:
+    from tornado.log import app_log
+except ImportError:
+    logging.basicConfig()
+    app_log = logging.getLogger()
+
+
 # IPython
 from IPython.kernel.multikernelmanager import MultiKernelManager
 
@@ -45,11 +52,6 @@ from IPython.frontend.html.notebook.notebookapp import (
 
 _kernel_id_regex = r"(?P<kernel_id>\w+)"
 
-class DummyIPythonApp(object):
-    """It's dumb that we need this"""
-    websocket_host = 'localhost:8000'
-
-
 class IndexHandler(web.RequestHandler):
     def get(self):
         return self.render('index.html')
@@ -65,7 +67,7 @@ class BesselHandler(web.RequestHandler):
 
 class WebApp(web.Application):
 
-    def __init__(self, kernel_manager, kernel_id, log):
+    def __init__(self, kernel_manager):
         handlers = [
             (r"/", IndexHandler),
             (r"/singlecell", SingleCellHandler),
@@ -90,18 +92,10 @@ class WebApp(web.Application):
             static_path='static',
             cookie_secret='secret',
             cookie_name='ignored',
+            kernel_manager=kernel_manager,
         )
 
         super(WebApp, self).__init__(handlers, **settings)
-
-        self.kernel_manager = kernel_manager
-        self.kernel_id = kernel_id
-        self.log = log
-        # unused stuff, required by our handlers
-        self.password = ''
-        self.read_only = False
-        self.ipython_app = DummyIPythonApp()
-        # self.config = self.ipython_app.config
 
 
 #-----------------------------------------------------------------------------
@@ -110,26 +104,20 @@ class WebApp(web.Application):
 
 def main():
     kernel_manager = MultiKernelManager()
-    # give the KernelManager attributes it shouldn't need,
-    # but IPython's handlers require:
-    kernel_manager.max_msg_size = 100*1024*1024
-    kernel_manager.time_to_dead = 1000
-    kernel_manager.first_beat = 1000
     
     # we are only using one kernel:
     kernel_id = '1'
     kernel_manager.start_kernel(kernel_id=kernel_id)
     
     logging.basicConfig(level=logging.INFO)
-    log = logging.getLogger()
-    app = WebApp(kernel_manager, kernel_id, log)
+    app = WebApp(kernel_manager)
     server = httpserver.HTTPServer(app)
     server.listen(8000, '127.0.0.1')
-    log.info("Serving at http://127.0.0.1:8000")
+    app_log.info("Serving at http://127.0.0.1:8000")
     try:
         ioloop.IOLoop.instance().start()
     except KeyboardInterrupt:
-        log.info("Interrupted...")
+        app_log.info("Interrupted...")
     finally:
         kernel_manager.shutdown_all()
 
